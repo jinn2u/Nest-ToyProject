48b106b12ed50e9d677e55e38a9d25983a57d4bf
jinn2u <bsybear623@gmail.com>
2021-01-10 17:04:07 +0900

    e2eTest

--- src/movies/app.controller.ts
+++ src/movies/app.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('app')
-export class AppController {}
+export class AppController {
+    @Get('/')
+    home():string{
+        return 'Welcome to my Movie API!'
+    }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,100 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+      //데코레이터가 없는 속성은 제거되어 서버로 들어간다.
+      whitelist: true,
+      //클라이언트에서 온 데이터 중 whitelist에 존재하지 않느 속성이 있다면(데코레이터가 없는속성이 제거된다.)httpException을 발생시킨다.
+      forbidNonWhitelisted: true,
+      transform: true,
+    }))
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/app')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API!');
   });
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+      .get('/movies')
+      .expect(200)
+      .expect([])
+    })
+    it('POST', () => {
+      return request(app.getHttpServer())
+      .post('/movies')
+      .send({
+        title: 'Test',
+        year: 2000,
+        genre:['Test']
+      })
+      .expect(201)
+    })
+    it('POST', () => {
+      return request(app.getHttpServer())
+      .post('/movies')
+      .send({
+        title: 'Test',
+        year: 2000,
+        genre:['Test']
+      })
+      .expect(201)
+    })
+    it('POST wrong json', () => {
+      return request(app.getHttpServer())
+      .post('/movies')
+      .send({
+        title: 'Test',
+        year: 2000,
+        genre:['Test'],
+        other: 'thing'
+      })
+      .expect(400)
+    })
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+      .delete('/movies')
+      .expect(404)
+    })
+  })
+  describe('movies/:id', () => {
+    it('GET 200', () => {
+    return request(app.getHttpServer())
+      .get('/movies/1')
+      .expect(200)
+    })
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
+        .expect(404)
+      })
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+      .patch('/movies/1')
+      .send({
+        title:"Updated Test"
+      })
+      .expect(200)
+    })
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+      .delete('/movies/1')
+      .expect(200)
+    })
+  })
 });
